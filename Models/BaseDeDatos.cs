f4d9f1f805f341f78b93f7bcde2f0fe90e2abb92
lucas Sechous <sechouslucas23@gmail.com>
2024-10-23 21:25:21 -0300

    1.4

--- Models/BaseDeDatos.cs
+++ Models/BaseDeDatos.cs
@@ -5,12 +5,11 @@ using System.Web;
 
 namespace Obligatorio.Models
 {
-    public class BaseDeDatos
+    public abstract class BaseDeDatos
     {
         
         public static List<Cliente> ListaClientes = new List<Cliente>();
         public static List<Tecnico> ListaTecnico = new List<Tecnico>();
-
 
 
 
