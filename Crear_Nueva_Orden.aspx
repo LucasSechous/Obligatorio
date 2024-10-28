cf0c8f8b5db75b6ebd9d167500abecba6d6c53c1
lucas Sechous <sechouslucas23@gmail.com>
2024-10-28 20:58:49 -0300

    1.8

--- Crear_Nueva_Orden.aspx
+++ Crear_Nueva_Orden.aspx
@@ -4,14 +4,33 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title></title>
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
 
+            <asp:Label ID="txtNombreCliente" runat="server" Text="Nombre del Cliente: "></asp:Label>
 
+            <asp:Label ID="txtNombreTecnico" runat="server" Text="Nombre del Cliente: "></asp:Label>
+            <%-- Hacer un dropdown --%>
+            <asp:DropDownList ID="seleccionar Tecnico" runat="server">
+
+                <asp:ListItem Enabled="true" Text="Seleccione un Tecnico" Value="-1"></asp:ListItem>
+                <asp:ListItem Value="Tec1"></asp:ListItem>
+                <asp:ListItem></asp:ListItem>
+                <asp:ListItem></asp:ListItem>
+
+
+            </asp:DropDownList>
+
+
+
+
+            <asp:Label ID="Label2" runat="server" Text="Nombre del Cliente: "></asp:Label>
+
+            <asp:Label ID="Label3" runat="server" Text="Nombre del Cliente: "></asp:Label>
 
 
 
--- Crear_Nueva_Orden.aspx.designer.cs
+++ Crear_Nueva_Orden.aspx.designer.cs
@@ -22,5 +22,50 @@ namespace Obligatorio
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control txtNombreCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label txtNombreCliente;
+
+        /// <summary>
+        /// Control txtNombreTecnico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label txtNombreTecnico;
+
+        /// <summary>
+        /// Control seleccionar Tecnico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList seleccionar Tecnico;
+        
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Control Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
     }
 }
