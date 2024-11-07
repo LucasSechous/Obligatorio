016d47487f51378b0590c259ff0a0da3895ead72
lucas Sechous <sechouslucas23@gmail.com>
2024-11-06 21:05:01 -0300

    2.8

---
+++ OrdenesWeb.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="OrdenesWeb.aspx.cs" Inherits="Obligatorio.WebForm1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title></title>
+    </div>
+    <main>
+        <div>
+
+            <asp:Label ID="lblNombreCliente" runat="server" Text="Nombre del Cliente: "></asp:Label>
+            <asp:TextBox ID="txtNombreCliente" runat="server" />
+        </div>
+        <div>
+            <asp:Label ID="lblTipoServicio" runat="server" Text="Servicio Deseado: "></asp:Label>
+
+            <%-- Hacer un dropdown --%>
+            <asp:DropDownList ID="ddlTipoServicio" runat="server">
+                <asp:ListItem ID="mostradoPrimero" runat="server" Enabled="true" Text="Seleccione un Servicio" Value="-1"></asp:ListItem>
+                <asp:ListItem Value="Montaje">Montaje</asp:ListItem>
+                <asp:ListItem Value="Sistemas">Sistemas</asp:ListItem>
+                <asp:ListItem Value="Reparacion">Reparacion</asp:ListItem>
+            </asp:DropDownList>
+        </div>
+        <div>
+            <asp:Calendar runat="server" ID="calendario"></asp:Calendar>
+            <table style="width: 100%;">
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
+            <asp:Table runat="server"></asp:Table>
+            <asp:Table ID="Table1" runat="server"></asp:Table>
+            &nbsp;
+        </div>
+    </main>
+
+</asp:Content>
---
+++ OrdenesWeb.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Obligatorio
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Control lblNombreCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNombreCliente;
+
+        /// <summary>
+        /// Control txtNombreCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombreCliente;
+
+        /// <summary>
+        /// Control lblTipoServicio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTipoServicio;
+
+        /// <summary>
+        /// Control ddlTipoServicio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlTipoServicio;
+
+        /// <summary>
+        /// Control mostradoPrimero.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListItem mostradoPrimero;
+
+        /// <summary>
+        /// Control calendario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Calendar calendario;
+
+        /// <summary>
+        /// Control Table1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table Table1;
+    }
+}
